3e9e5f5c530d4b13c1c63cc5930edcc51f445072
amicablecoder9 <52830604+amicablecoder9@users.noreply.github.com>
2019-11-24 15:18:10 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
 module.exports = {
   mongoURI: "mongodb+srv://anky_9:2498anky@whitepanda-knl8b.mongodb.net/test?retryWrites=true&w=majority",
-  twilioaccountSID: "AC1af5d64105a78a468d82cbf064ca847f",
-  twilioauthToken: "97ec985fb841f4c821d2a5c6c12559a7",
   serviceSID: null,
   secretOrKey: "secret"
 };
